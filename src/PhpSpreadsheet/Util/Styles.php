b5907a83244ece73680c826c18c837136c700408
Paul Gomez <pauldro952@outlook.com>
2026-06-08 16:23:53 -0500

    Update Styles.php

--- src/PhpSpreadsheet/Util/Styles.php
+++ src/PhpSpreadsheet/Util/Styles.php
@@ -1,4 +1,4 @@
-<?php namespace Pauldro\Minicli\Spreadsheet\PhpSpreadsheet;
+<?php namespace Pauldro\Minicli\Spreadsheet\PhpSpreadsheet\Util;
 // PhpSpreadsheet
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
